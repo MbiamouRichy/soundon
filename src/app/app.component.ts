f627db8c21733e5d5670857c23319d34eb77456a
Richy <richymbiamou52@gmail.com>
2023-02-24 22:50:37 +0100

    fin du projet modif de l'image 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,29 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'soundon';
   ngOnInit(){
-    let btn_image1 = document.querySelectorAll('nav .div_image .btn_image1');
-    btn_image1.forEach(img => img.addEventListener('click', () =>{
-      let i = img.id;
-      let bg = document.querySelector('.bg');
-      // @ts-ignore
-      let chg: string | null;
 
-      if (i == "1"){
-        chg = "assets/D00qO4pA.jpg"
-      }else
-      if(i == "2"){
-        chg = "assets/JAAVOt48.jpg"
-      }else {
-        chg = "assets/N8LsPSeF.jpg"
-      }
-      // @ts-ignore
-      bg.setAttribute("src", chg)
-    }))
-    if (document.body.offsetWidth < 1024){
-     let br = document.querySelector('.texte br')
-      // @ts-ignore
-      br.style.display='none'
-    }
   }
 }
 
